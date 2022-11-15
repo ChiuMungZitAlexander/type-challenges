300f4ac9742681b98ac16ca3955c291bfe9095a8
AlexanderZhao <zhaomengzhe@gmail.com>
2022-11-15 09:20:18 +0800

    feat(): first of array

---
+++ src/first-of-array/index.ts
@@ -0,0 +1,17 @@
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type First<T extends any[]> = T["length"] extends 0 ? never : T[0];
+
+type cases = [
+  Expect<Equal<First<[3, 2, 1]>, 3>>,
+  Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
+  Expect<Equal<First<[]>, never>>,
+  Expect<Equal<First<[undefined]>, undefined>>
+];
+
+type errors = [
+  // @ts-expect-error
+  First<"notArray">,
+  // @ts-expect-error
+  First<{ 0: "arrayLike" }>
+];
