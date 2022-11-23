8c9077c7673af51947682a0dd7962837f1268cad
AlexanderZhao <zhaomengzhe@gmail.com>
2022-11-23 09:36:06 +0800

    feat(): push

---
+++ src/push/index.ts
@@ -0,0 +1,9 @@
+type Push<T extends unknown[], U> = [...T, U];
+
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<Push<[], 1>, [1]>>,
+  Expect<Equal<Push<[1, 2], "3">, [1, 2, "3"]>>,
+  Expect<Equal<Push<["1", 2, "3"], boolean>, ["1", 2, "3", boolean]>>
+];
