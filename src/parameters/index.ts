52de2ceed4b9c3744d8ae752aab5589f47315c6a
AlexanderZhao <zhaomengzhe@gmail.com>
2022-11-24 09:17:10 +0800

    feat(): parameters

---
+++ src/parameters/index.ts
@@ -0,0 +1,13 @@
+type MyParameters<T> = T extends (...args: infer U) => any ? U : never;
+
+import type { Equal, Expect } from "@type-challenges/utils";
+
+const foo = (arg1: string, arg2: number): void => {};
+const bar = (arg1: boolean, arg2: { a: "A" }): void => {};
+const baz = (): void => {};
+
+type cases = [
+  Expect<Equal<MyParameters<typeof foo>, [string, number]>>,
+  Expect<Equal<MyParameters<typeof bar>, [boolean, { a: "A" }]>>,
+  Expect<Equal<MyParameters<typeof baz>, []>>
+];
