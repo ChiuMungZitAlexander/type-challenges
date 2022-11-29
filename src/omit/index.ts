55b640ae6d08e9d725e38dbe2fcc9a58838ffa52
AlexanderZhao <zhaomengzhe@gmail.com>
2022-11-29 09:37:00 +0800

    feat(): omit

---
+++ src/omit/index.ts
@@ -0,0 +1,28 @@
+type MyOmit<T, K extends keyof T> = {
+  [key in keyof T as key extends K ? never : key]: T[key];
+};
+
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<Expected1, MyOmit<Todo, "description">>>,
+  Expect<Equal<Expected2, MyOmit<Todo, "description" | "completed">>>
+];
+
+// @ts-expect-error
+type error = MyOmit<Todo, "description" | "invalid">;
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+interface Expected1 {
+  title: string;
+  completed: boolean;
+}
+
+interface Expected2 {
+  title: string;
+}
