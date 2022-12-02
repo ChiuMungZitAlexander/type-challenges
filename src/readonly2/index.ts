637b483a482580ba1d7585627eed19e5d6e27f68
AlexanderZhao <zhaomengzhe@gmail.com>
2022-12-02 09:19:49 +0800

    feat(): readonly2

---
+++ src/readonly2/index.ts
@@ -0,0 +1,34 @@
+type MyReadonly2<T, U extends keyof T = keyof T> = {
+  readonly [key in U]: T[key];
+} & {
+  [key in Exclude<keyof T, U>]: T[key];
+};
+
+import type { Alike, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Alike<MyReadonly2<Todo1>, Readonly<Todo1>>>,
+  Expect<Alike<MyReadonly2<Todo1, "title" | "description">, Expected>>,
+  Expect<Alike<MyReadonly2<Todo2, "title" | "description">, Expected>>
+];
+
+// @ts-expect-error
+type error = MyReadonly2<Todo1, "title" | "invalid">;
+
+interface Todo1 {
+  title: string;
+  description?: string;
+  completed: boolean;
+}
+
+interface Todo2 {
+  readonly title: string;
+  description?: string;
+  completed: boolean;
+}
+
+interface Expected {
+  readonly title: string;
+  readonly description?: string;
+  completed: boolean;
+}
