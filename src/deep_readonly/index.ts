910106d1c4e14b24b9d1e39967a65885ed8de4c4
AlexanderZhao <zhaomengzhe@gmail.com>
2022-12-05 09:42:17 +0800

    feat(): DeepReadonly

---
+++ src/deep_readonly/index.ts
@@ -0,0 +1,60 @@
+type DeepReadonly<T> = {
+  readonly [key in keyof T]: T[key] extends
+    | Function
+    | string
+    | number
+    | undefined
+    | null
+    ? T[key]
+    : DeepReadonly<T[key]>;
+};
+
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [Expect<Equal<DeepReadonly<X>, Expected>>];
+
+type X = {
+  a: () => 22;
+  b: string;
+  c: {
+    d: boolean;
+    e: {
+      g: {
+        h: {
+          i: true;
+          j: "string";
+        };
+        k: "hello";
+      };
+      l: [
+        "hi",
+        {
+          m: ["hey"];
+        }
+      ];
+    };
+  };
+};
+
+type Expected = {
+  readonly a: () => 22;
+  readonly b: string;
+  readonly c: {
+    readonly d: boolean;
+    readonly e: {
+      readonly g: {
+        readonly h: {
+          readonly i: true;
+          readonly j: "string";
+        };
+        readonly k: "hello";
+      };
+      readonly l: readonly [
+        "hi",
+        {
+          readonly m: readonly ["hey"];
+        }
+      ];
+    };
+  };
+};
