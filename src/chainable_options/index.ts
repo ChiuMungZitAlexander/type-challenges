4737f05f245e62dc64d8187ef83f2e87badcc287
AlexanderZhao <zhaomengzhe@gmail.com>
2022-12-12 09:28:46 +0800

    feat(): chainable options

---
+++ src/chainable_options/index.ts
@@ -0,0 +1,51 @@
+type Chainable<P = {}> = {
+  option<T extends string, K>(
+    key: T extends keyof P ? never : T,
+    value: K
+  ): Chainable<Omit<P, T> & { [key in T]: K }>;
+  get(): P;
+};
+
+import type { Alike, Expect } from "@type-challenges/utils";
+
+declare const a: Chainable;
+
+const result1 = a
+  .option("foo", 123)
+  .option("bar", { value: "Hello World" })
+  .option("name", "type-challenges")
+  .get();
+
+const result2 = a
+  .option("name", "another name")
+  // @ts-expect-error
+  .option("name", "last name")
+  .get();
+
+const result3 = a
+  .option("name", "another name")
+  // @ts-expect-error
+  .option("name", 123)
+  .get();
+
+type cases = [
+  Expect<Alike<typeof result1, Expected1>>,
+  Expect<Alike<typeof result2, Expected2>>,
+  Expect<Alike<typeof result3, Expected3>>
+];
+
+type Expected1 = {
+  foo: number;
+  bar: {
+    value: string;
+  };
+  name: string;
+};
+
+type Expected2 = {
+  name: string;
+};
+
+type Expected3 = {
+  name: number;
+};
