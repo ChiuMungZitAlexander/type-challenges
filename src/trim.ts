043cf93a770487db8474f0b721c64ae2c06fcd60
AlexanderZhao <zhaomengzhe@gmail.com>
2022-12-22 13:04:02 +0800

    feat(): trim

---
+++ src/trim.ts
@@ -0,0 +1,18 @@
+type Trim<S extends string> = S extends
+  | `${" " | "\n" | "\t"}${infer Rest}`
+  | `${infer Rest}${" " | "\n" | "\t"}`
+  ? Trim<Rest>
+  : S;
+
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<Trim<"str">, "str">>,
+  Expect<Equal<Trim<" str">, "str">>,
+  Expect<Equal<Trim<"     str">, "str">>,
+  Expect<Equal<Trim<"str   ">, "str">>,
+  Expect<Equal<Trim<"     str     ">, "str">>,
+  Expect<Equal<Trim<"   \n\t foo bar \t">, "foo bar">>,
+  Expect<Equal<Trim<"">, "">>,
+  Expect<Equal<Trim<" \n\t ">, "">>
+];
