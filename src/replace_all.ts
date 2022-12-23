821146f6806db259e5da162c70f18e39b0de4125
AlexanderZhao <zhaomengzhe@gmail.com>
2022-12-23 21:06:58 +0800

    feat(): replace all

---
+++ src/replace_all.ts
@@ -0,0 +1,23 @@
+type ReplaceAll<
+  S extends string,
+  From extends string,
+  To extends string
+> = From extends ""
+  ? S
+  : S extends `${infer Head}${From}${infer Tail}`
+  ? `${Head}${To}${ReplaceAll<Tail, From, To>}`
+  : S;
+
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<ReplaceAll<"foobar", "bar", "foo">, "foofoo">>,
+  Expect<Equal<ReplaceAll<"foobar", "bag", "foo">, "foobar">>,
+  Expect<Equal<ReplaceAll<"foobarbar", "bar", "foo">, "foofoofoo">>,
+  Expect<Equal<ReplaceAll<"t y p e s", " ", "">, "types">>,
+  Expect<Equal<ReplaceAll<"foobarbar", "", "foo">, "foobarbar">>,
+  Expect<Equal<ReplaceAll<"barfoo", "bar", "foo">, "foofoo">>,
+  Expect<Equal<ReplaceAll<"foobarfoobar", "ob", "b">, "fobarfobar">>,
+  Expect<Equal<ReplaceAll<"foboorfoboar", "bo", "b">, "foborfobar">>,
+  Expect<Equal<ReplaceAll<"", "", "">, "">>
+];
