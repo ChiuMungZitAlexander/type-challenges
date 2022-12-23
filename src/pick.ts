b945919baff9ace54cb48cb149928a8067b7cf27
AlexanderZhao <zhaomengzhe@gmail.com>
2022-12-23 21:10:45 +0800

    feat(): pick

--- src/pick.ts
+++ src/pick.ts
@@ -1,4 +1,15 @@
+type MyPick<T, K extends keyof T> = {
+  [key in K]: T[key];
+};
+
 import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<Expected1, MyPick<Todo, "title">>>,
+  Expect<Equal<Expected2, MyPick<Todo, "title" | "completed">>>,
+  // @ts-expect-error
+  MyPick<Todo, "title" | "completed" | "invalid">
+];
 
 interface Todo {
   title: string;
@@ -14,14 +25,3 @@ interface Expected2 {
   title: string;
   completed: boolean;
 }
-
-type MyPick<T, K extends keyof T> = {
-  [key in K]: T[key];
-};
-
-type cases = [
-  Expect<Equal<Expected1, MyPick<Todo, "title">>>,
-  Expect<Equal<Expected2, MyPick<Todo, "title" | "completed">>>,
-  // @ts-expect-error
-  MyPick<Todo, "title" | "completed" | "invalid">
-];
