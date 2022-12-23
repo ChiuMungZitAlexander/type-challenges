591478fe37046ce64450d2a0e0d4de85963609aa
AlexanderZhao <zhaomengzhe@gmail.com>
2022-12-23 21:30:48 +0800

    feat(): tuple to object

--- src/tuple_to_object.ts
+++ src/tuple_to_object.ts
@@ -1,7 +1,30 @@
-const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
-
-type TupleToObject<T extends readonly any[]> = {
+type TupleToObject<T extends readonly (string | number)[]> = {
   [key in T[number]]: key;
-}
+};
 
-type result = TupleToObject<typeof tuple> // expected { tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
+import type { Equal, Expect } from "@type-challenges/utils";
+
+const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
+const tupleNumber = [1, 2, 3, 4] as const;
+const tupleMix = [1, "2", 3, "4"] as const;
+
+type cases = [
+  Expect<
+    Equal<
+      TupleToObject<typeof tuple>,
+      {
+        tesla: "tesla";
+        "model 3": "model 3";
+        "model X": "model X";
+        "model Y": "model Y";
+      }
+    >
+  >,
+  Expect<Equal<TupleToObject<typeof tupleNumber>, { 1: 1; 2: 2; 3: 3; 4: 4 }>>,
+  Expect<
+    Equal<TupleToObject<typeof tupleMix>, { 1: 1; "2": "2"; 3: 3; "4": "4" }>
+  >
+];
+
+// @ts-expect-error
+type error = TupleToObject<[[1, 2], {}]>;
