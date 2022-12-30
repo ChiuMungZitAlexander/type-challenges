c19892f53503e26d3a6b7d48a0ade7ae3083dd04
AlexanderZhao <zhaomengzhe@gmail.com>
2022-12-30 18:21:39 +0800

    feat(): permutation

---
+++ src/permutation.ts
@@ -0,0 +1,36 @@
+type Permutation<T, U = T> = [T] extends [never]
+  ? []
+  : T extends U
+  ? [T, ...Permutation<Exclude<U, T>>]
+  : never;
+
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<Permutation<"A">, ["A"]>>,
+  Expect<Equal<Permutation<"A" | "B">, ["A", "B"] | ["B", "A"]>>,
+  Expect<
+    Equal<
+      Permutation<"A" | "B" | "C">,
+      | ["A", "B", "C"]
+      | ["A", "C", "B"]
+      | ["B", "A", "C"]
+      | ["B", "C", "A"]
+      | ["C", "A", "B"]
+      | ["C", "B", "A"]
+    >
+  >,
+  Expect<
+    Equal<
+      Permutation<"B" | "A" | "C">,
+      | ["A", "B", "C"]
+      | ["A", "C", "B"]
+      | ["B", "A", "C"]
+      | ["B", "C", "A"]
+      | ["C", "A", "B"]
+      | ["C", "B", "A"]
+    >
+  >,
+  Expect<Equal<Permutation<boolean>, [false, true] | [true, false]>>,
+  Expect<Equal<Permutation<never>, []>>
+];
