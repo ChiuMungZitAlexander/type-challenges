bcb1298bab860045ead5c90b1663874719e7809b
AlexanderZhao <zhaomengzhe@gmail.com>
2023-01-04 09:15:57 +0800

    feat(): absolute

---
+++ src/absolute.ts
@@ -0,0 +1,21 @@
+type Absolute<T extends number | string | bigint> =
+  `${T}` extends `${infer First}${infer Rest}`
+    ? First extends "-"
+      ? Rest
+      : `${T}`
+    : never;
+
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<Absolute<0>, "0">>,
+  Expect<Equal<Absolute<-0>, "0">>,
+  Expect<Equal<Absolute<10>, "10">>,
+  Expect<Equal<Absolute<-5>, "5">>,
+  Expect<Equal<Absolute<"0">, "0">>,
+  Expect<Equal<Absolute<"-0">, "0">>,
+  Expect<Equal<Absolute<"10">, "10">>,
+  Expect<Equal<Absolute<"-5">, "5">>,
+  Expect<Equal<Absolute<-1_000_000n>, "1000000">>,
+  Expect<Equal<Absolute<9_999n>, "9999">>
+];
