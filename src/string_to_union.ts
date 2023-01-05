e01689641f294a248f211096318795eec8bbc98e
AlexanderZhao <zhaomengzhe@gmail.com>
2023-01-05 09:08:11 +0800

    feat(): string to union

---
+++ src/string_to_union.ts
@@ -0,0 +1,17 @@
+type StringToUnion<T extends string> = T extends `${infer First}${infer Rest}`
+  ? First | StringToUnion<Rest>
+  : never;
+
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<StringToUnion<"">, never>>,
+  Expect<Equal<StringToUnion<"t">, "t">>,
+  Expect<Equal<StringToUnion<"hello">, "h" | "e" | "l" | "l" | "o">>,
+  Expect<
+    Equal<
+      StringToUnion<"coronavirus">,
+      "c" | "o" | "r" | "o" | "n" | "a" | "v" | "i" | "r" | "u" | "s"
+    >
+  >
+];
