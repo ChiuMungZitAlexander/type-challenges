d5e098df3f6d1b77707b8462eeba32649f5098b5
AlexanderZhao <zhaomengzhe@gmail.com>
2023-01-10 10:17:20 +0800

    feat(): kabab case

---
+++ src/kabab_case.ts
@@ -0,0 +1,24 @@
+type KebabCase<
+  S,
+  C extends boolean = true
+> = S extends `${infer First}${infer Rest}`
+  ? First extends "A" | "B" | "C" | "D" | "E" | "F" | "G"
+    ? C extends true
+      ? `${Lowercase<First>}${KebabCase<Rest, false>}`
+      : `-${Lowercase<First>}${KebabCase<Rest, false>}`
+    : `${First}${KebabCase<Rest, false>}`
+  : "";
+
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<KebabCase<"FooBarBaz">, "foo-bar-baz">>,
+  Expect<Equal<KebabCase<"fooBarBaz">, "foo-bar-baz">>,
+  Expect<Equal<KebabCase<"foo-bar">, "foo-bar">>,
+  Expect<Equal<KebabCase<"foo_bar">, "foo_bar">>,
+  Expect<Equal<KebabCase<"Foo-Bar">, "foo--bar">>,
+  Expect<Equal<KebabCase<"ABC">, "a-b-c">>,
+  Expect<Equal<KebabCase<"-">, "-">>,
+  Expect<Equal<KebabCase<"">, "">>,
+  Expect<Equal<KebabCase<"😎">, "😎">>
+];
