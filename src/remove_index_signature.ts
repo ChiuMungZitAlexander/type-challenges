4931240f2f04001d82e04b32d852847d94c15e91
AlexanderZhao <zhaomengzhe@gmail.com>
2023-01-17 09:20:24 +0800

    feat(): remove index signature

---
+++ src/remove_index_signature.ts
@@ -0,0 +1,34 @@
+type RemoveIndexSignature<T extends {}> = {
+  [key in keyof T as {} extends Record<key, any> ? never : key]: T[key];
+};
+
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type Foo = {
+  [key: string]: any;
+  foo(): void;
+};
+
+type Bar = {
+  [key: number]: any;
+  bar(): void;
+  0: string;
+};
+
+const foobar = Symbol("foobar");
+type FooBar = {
+  [key: symbol]: any;
+  [foobar](): void;
+};
+
+type Baz = {
+  bar(): void;
+  baz: string;
+};
+
+type cases = [
+  Expect<Equal<RemoveIndexSignature<Foo>, { foo(): void }>>,
+  Expect<Equal<RemoveIndexSignature<Bar>, { bar(): void; 0: string }>>,
+  Expect<Equal<RemoveIndexSignature<FooBar>, { [foobar](): void }>>,
+  Expect<Equal<RemoveIndexSignature<Baz>, { bar(): void; baz: string }>>
+];
