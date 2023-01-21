644c7d64230c36cac939ee9aabb4453b18a0b494
AlexanderZhao <zhaomengzhe@qq.com>
2023-01-21 19:24:11 +0800

    feat(): drop char

---
+++ src/drop_char.ts
@@ -0,0 +1,19 @@
+type DropChar<
+  S extends string,
+  C extends string
+> = S extends `${infer Head}${infer Tail}`
+  ? `${Head extends C ? "" : Head}${DropChar<Tail, C>}`
+  : "";
+
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  // @ts-expect-error
+  Expect<Equal<DropChar<"butter fly!", "">, "butterfly!">>,
+  Expect<Equal<DropChar<"butter fly!", " ">, "butterfly!">>,
+  Expect<Equal<DropChar<"butter fly!", "!">, "butter fly">>,
+  Expect<Equal<DropChar<"    butter fly!        ", " ">, "butterfly!">>,
+  Expect<Equal<DropChar<" b u t t e r f l y ! ", " ">, "butterfly!">>,
+  Expect<Equal<DropChar<" b u t t e r f l y ! ", "b">, "  u t t e r f l y ! ">>,
+  Expect<Equal<DropChar<" b u t t e r f l y ! ", "t">, " b u   e r f l y ! ">>
+];
