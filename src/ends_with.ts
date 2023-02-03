bb69ea17d53b6ac0df08fab21e2b6377e02612c1
AlexanderZhao <zhaomengzhe@gmail.com>
2023-02-03 10:30:51 +0800

    feat(): ends with

---
+++ src/ends_with.ts
@@ -0,0 +1,14 @@
+type EndsWith<T extends string, U extends string> = T extends `${string}${U}`
+  ? true
+  : false;
+
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<EndsWith<"abc", "bc">, true>>,
+  Expect<Equal<EndsWith<"abc", "abc">, true>>,
+  Expect<Equal<EndsWith<"abc", "d">, false>>,
+  Expect<Equal<EndsWith<"abc", "ac">, false>>,
+  Expect<Equal<EndsWith<"abc", "">, true>>,
+  Expect<Equal<EndsWith<"abc", " ">, false>>
+];
