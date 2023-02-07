92567a8dcce0b067bcc379abdafc15581489db9a
AlexanderZhao <zhaomengzhe@gmail.com>
2023-02-07 09:15:08 +0800

    feat(): required by keys

---
+++ src/required_by_keys.ts
@@ -0,0 +1,34 @@
+type RequiredByKeys<T, K extends keyof T = keyof T> = {
+  [key in keyof (Omit<T, K> & Required<Pick<T, K>>)]: Exclude<
+    T[key],
+    undefined
+  >;
+};
+
+import type { Equal, Expect } from "@type-challenges/utils";
+
+interface User {
+  name?: string;
+  age?: number;
+  address?: string;
+}
+
+interface UserRequiredName {
+  name: string;
+  age?: number;
+  address?: string;
+}
+
+interface UserRequiredNameAndAge {
+  name: string;
+  age: number;
+  address?: string;
+}
+
+type cases = [
+  Expect<Equal<RequiredByKeys<User, "name">, UserRequiredName>>,
+  Expect<Equal<RequiredByKeys<User, "name" | "age">, UserRequiredNameAndAge>>,
+  Expect<Equal<RequiredByKeys<User>, Required<User>>>,
+  // @ts-expect-error
+  Expect<Equal<RequiredByKeys<User, "name" | "unknown">, UserRequiredName>>
+];
