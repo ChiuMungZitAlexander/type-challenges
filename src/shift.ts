974748bdcd04b630f43fbca1b80977d9001e8c1e
AlexanderZhao <zhaomengzhe@gmail.com>
2023-02-11 19:41:02 +0800

    feat(): shift

---
+++ src/shift.ts
@@ -0,0 +1,14 @@
+type Shift<T extends any[]> = T extends [infer First, ...infer Rest]
+  ? Rest
+  : [];
+
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  // @ts-expect-error
+  Shift<unknown>,
+  Expect<Equal<Shift<[]>, []>>,
+  Expect<Equal<Shift<[1]>, []>>,
+  Expect<Equal<Shift<[3, 2, 1]>, [2, 1]>>,
+  Expect<Equal<Shift<["a", "b", "c", "d"]>, ["b", "c", "d"]>>
+];
