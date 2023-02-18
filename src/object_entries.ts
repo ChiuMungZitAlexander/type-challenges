482007703ed123741ee24e30117006bb0887ffde
AlexanderZhao <zhaomengzhe@gmail.com>
2023-02-18 20:34:15 +0800

    feat(): object entries

---
+++ src/object_entries.ts
@@ -0,0 +1,23 @@
+type ObjectEntries<T, K extends keyof T = keyof T> = K extends keyof T
+  ? [K, Required<T>[K] extends undefined ? undefined : Required<T>[K]]
+  : never;
+
+import type { Equal, Expect } from "@type-challenges/utils";
+
+interface Model {
+  name: string;
+  age: number;
+  locations: string[] | null;
+}
+
+type ModelEntries =
+  | ["name", string]
+  | ["age", number]
+  | ["locations", string[] | null];
+
+type cases = [
+  Expect<Equal<ObjectEntries<Model>, ModelEntries>>,
+  Expect<Equal<ObjectEntries<Partial<Model>>, ModelEntries>>,
+  Expect<Equal<ObjectEntries<{ key?: undefined }>, ["key", undefined]>>,
+  Expect<Equal<ObjectEntries<{ key: undefined }>, ["key", undefined]>>
+];
