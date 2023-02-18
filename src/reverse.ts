2b0dd0e13af37b72680394d3a55e1711b109cd88
AlexanderZhao <zhaomengzhe@gmail.com>
2023-02-18 20:34:27 +0800

    feat(): reverse

---
+++ src/reverse.ts
@@ -0,0 +1,21 @@
+type Reverse<T extends any[], U extends any[] = []> = T extends [
+  infer First,
+  ...infer Rest
+]
+  ? Reverse<Rest, [First, ...U]>
+  : U;
+
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<Reverse<[]>, []>>,
+  Expect<Equal<Reverse<["a", "b"]>, ["b", "a"]>>,
+  Expect<Equal<Reverse<["a", "b", "c"]>, ["c", "b", "a"]>>
+];
+
+type errors = [
+  // @ts-expect-error
+  Reverse<"string">,
+  // @ts-expect-error
+  Reverse<{ key: "value" }>
+];
